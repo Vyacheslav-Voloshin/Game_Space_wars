49b12615afd6198f592ede67c712879b4743e79f
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-10-01 22:39:33 +0300

    created methods  in class Ufo

--- src/project/SpaceShip.java
+++ src/project/SpaceShip.java
@@ -9,7 +9,15 @@ package project;
     якщо dx = -1, корабель кожен хід йде на 1 ліворуч.
      */
     public class SpaceShip extends BaseObject{
-        private double dx = 0;
+        //картинка корабля для малювання на екрані
+        private static int[][] matrix = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {1, 0, 1, 0, 1},
+                {1, 1, 1, 1, 1},
+        };
+        private double dx = 0; //зміна для переміщення корабля вліво та вправо
         public SpaceShip(double x, double y) {
 
             super(x, y, 3);
@@ -25,16 +33,17 @@ package project;
             dx = 1;
         }
 
-        // метод встановлює точку на екрані с координатами х та у в кольор "В"
+        // метод встановлює точку на екрані с координатами х та у в кольор "M"
         @Override
         public void draw(Canvas canvas) {
-
+            canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'M');
         }
         //метод переміщує корабель та перевіряє чи не виліз він за кордони космосу
         @Override
         public void move() {
             x+=dx;
-            checkBorders(0,Space.game.getWidth(),0,Space.game.getHeight() );
+            checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+
         }
 
         //цей метод викликається коли необхідно здійснити постріл, корабель стріляє двома ракетами одночасно
--- src/project/Ufo.java
+++ src/project/Ufo.java
@@ -1,7 +1,50 @@
 package project;
 
-public class Ufo extends BaseObject{
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+public class Ufo extends BaseObject {
+    //картинка НЛО для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    public Ufo(double x, double y) {
+
+        super(x, y, 3);
+    }
+
+    //// метод встановлює точку на екрані с координатами х та у в кольор "U"
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    /*
+    Метод move() треба зробити так:
+а) UFO переміщається випадковою траєкторією  (не забудь про перевірку кордонів);
+б) UFO не опускається у нижню половину екрана;
+y <= height/2
+в) З ймовірністю 10% UFO повинен стріляти – викликати метод fire.
+     */
+    @Override
+    public void move() {
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
+
+        x += dx;
+        y += dy;
+
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius - 1, Space.game.getHeight() / 2 - 1);
+        double rundomFire =  Math.random()*100;
+        if (rundomFire<=10){
+            fire();
+        }
+    }
+
+    //цей метод викликається коли НЛО скидає бомбу
+    public void fire(){
+        Space.game.getBombs().add(new Bomb(x, y+3));
     }
 }
